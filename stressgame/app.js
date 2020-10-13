3b5c7d2625fba854f615a9f9297ec389f2ee8299
Rloui <69759725+Rloui@users.noreply.github.com>
2020-10-13 11:37:20 -0400

    deckSizer Completed

--- stressgame/app.js
+++ stressgame/app.js
@@ -12,7 +12,7 @@
 ////////////////
 // Class Deck, creates outline for deck of cards
 ////////////////
-class Deck {
+class Card {
     constructor (suit, rank, color) {
         this.suit = suit
         this.rank = rank
@@ -32,41 +32,62 @@ const cardColor = ['black', 'red']
 // ranks: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
 const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-////////////////
-// Array to hold deck
-////////////////
-// create an array to hold the deck
+// this array to holds the deck
 const deck = []
 
 ////////////////
 // Function creates deck
 ////////////////
-// create a render deck function
 const createDeck = () => {
     // use a loop to fill the deck array with card objects
-    // first loop give card is color
-    // for (let i = 0; i < cardColor.length; i++) {
-        // first loop creates the card suit
-        for (let j = 0; j < suits.length; j++) {
-            // second loop creates the card value
-            for (let k = 0; k < ranks.length; k++) {
-                if(suits[j] === 'clubs' || suits[j] === 'spades') {
-                    let suitColor = cardColor[0]
-                    const deckCreation = new Deck(suits[j], ranks[k], suitColor)
-                    deck.push(deckCreation)
-                } else {
-                    suitColor = cardColor[1]
-                    const deckCreation = new Deck(suits[j], ranks[k], suitColor)
-                    deck.push(deckCreation)
+    // first loop creates the card suit
+    for (let j = 0; j < suits.length; j++) {
+        // second loop creates the card value
+        for (let k = 0; k < ranks.length; k++) {
+            // if else statement specifies the color of the card
+            if(suits[j] === 'clubs' || suits[j] === 'spades') {
+            let suitColor = cardColor[0]
+            const deckCreation = new Card(suits[j], ranks[k], suitColor)
+                deck.push(deckCreation)
+            } else {
+                suitColor = cardColor[1]
+                const deckCreation = new Card(suits[j], ranks[k], suitColor)
+                deck.push(deckCreation)
+            }
+        }
+    }
+}
+// a deck of 52 cards are created
+createDeck() 
+
+////////////////
+// Function re sizes deck baised on playing mode
+////////////////
+const deckSizer = () => {
+    let playersMode = prompt('what mode do you want to play in Mult-player or Single player? Type Single or Mult below')
+    // resizes deck for multi player mode elss if single player mode
+    if (playersMode === 'Mult' || playersMode === 'mult') {
+        return deck
+    } else if (playersMode === 'Single' || playersMode === "single") {
+        for (let i = 0; i < deck.length; i++) {
+            if (i === 7 || i === 14 || i === 21 || i === 28) {
+                for (let j = 0; j < 6; j++){
+                    deck.splice(i, 1)
                 }
             }
         }
-    // }
+        return deck
+    }
 }
-createDeck()
+let playingDeck = deckSizer()
+console.log(playingDeck)
 
-
+////////////////
+// Function re sizes deck baised on playing mode
+////////////////
 //  create a shuffle function
+
+
 
 // create a deal function
     // for multiplayer or single player
