07da4f1f8a390d0c3cf23cb583ed4b684471e2b1
Rloui <69759725+Rloui@users.noreply.github.com>
2020-11-03 14:24:07 -0500

    Set up Schema

---
+++ BlkBusinneses/models/businesses.js
@@ -0,0 +1,24 @@
+// ************
+// Dependencies
+// ************
+const mongoose = require('mongoose')
+
+// Mongoose Schema Variable
+const Schema = mongoose.Schema
+
+// Create Schema
+const businessSchema = new Schema ({
+    name: {type: String, required: true},
+    location: String,
+    owners: {type: String, required: true},
+    type: String,
+    phone: String,
+    email: String,
+    website: String,
+    image: String
+}, timestamps: true)
+
+// Create model
+const business = mongoose.model('business', businessSchema)
+
+module.exports = business
