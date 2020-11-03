a8a3e706191d9233feb506ff7a9aad5f96061413
Rloui <69759725+Rloui@users.noreply.github.com>
2020-11-03 14:45:40 -0500

    Server Set Up

--- BlkBusinneses/server.js
+++ BlkBusinneses/server.js
@@ -1,3 +1,44 @@
+// ************
+// Dependencies
+// ************
 const express = require('express')
 const mongoose = require('mongoose')
-const 
+const businessController = require('./controllers/router.js')
+const app = express()
+const PORT = 3000
+
+// ************
+// Middleware
+// ************
+
+// Use Controller
+app.use('blk', businessController)
+
+// ************
+// Globl Configuration
+// ************
+const mongoURI = 'mongodb://localhost:27017/' + 'business'
+const db = mongoose.connection
+
+// Connect to Mongo
+mongoose.connect(
+	mongoURI, {
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}, () => {
+		console.log('the connection with mongod is established')
+	}
+)
+
+// Connection Error/Success
+// Define callback functions for various events
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+
+// Server PORT
+app.listen(PORT, () => {
+    console.log('BLK Server is listening ' + PORT)
+})
