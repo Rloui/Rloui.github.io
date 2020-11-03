dac5cee1d8661238a9d1a5f3a7a46ff1bf3678d2
Rloui <69759725+Rloui@users.noreply.github.com>
2020-11-03 14:38:22 -0500

    Basic Router set up

---
+++ BlkBusinneses/controllers/router.js
@@ -0,0 +1,17 @@
+// ************
+// Dependencies
+// ************
+const express = require('express')
+const Business = require('../models/businesses')
+
+// ************
+// Middleware
+// ************
+
+
+// ************
+// Globl Configuration
+// ************
+const router = express.Router()
+
+module.exports = router
